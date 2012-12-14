491fd437f065cb99f7797d9c2dae44e01a77dd91
Simon <yqqjerk@yangmatoMacBook-Pro.local>
2012-12-14 13:38:20 +0800

    flight search basic ui

--- TaurusClient/FlightSearch/FlightSearchViewController.h
+++ TaurusClient/FlightSearch/FlightSearchViewController.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FlightSearchViewController : UIViewController
+@interface FlightSearchViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, retain) IBOutlet UIView*			singleFlightParentView;
+@property (nonatomic, retain) IBOutlet UIView*			doubleFlightParentView;
+@property (nonatomic, retain) IBOutlet UITableView*		singleFlightTableView;
+@property (nonatomic, retain) IBOutlet UITableView*		doubleFlightTableView;
+
+- (IBAction)onSwitchToSingleFlightButtonTap:(id)sender;
+- (IBAction)onSwitchToDoubleFlightButtonTap:(id)sender;
 
 @end
--- TaurusClient/FlightSearch/FlightSearchViewController.m
+++ TaurusClient/FlightSearch/FlightSearchViewController.m
@@ -7,12 +7,24 @@
 //
 
 #import "FlightSearchViewController.h"
+#import "UIViewAdditions.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface FlightSearchViewController ()
 
 @end
 
 @implementation FlightSearchViewController
+
+- (void)dealloc
+{
+	self.singleFlightParentView = nil;
+	self.singleFlightTableView = nil;
+	self.doubleFlightParentView = nil;
+	self.doubleFlightTableView = nil;
+	
+	[super dealloc];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,12 +38,84 @@
 {
     [super viewDidLoad];
 	self.title = @"机票搜索";
+	
+	[self.view addSubview:self.singleFlightParentView];
+	[self.view addSubview:self.doubleFlightParentView];
+	
+	self.doubleFlightParentView.left = self.singleFlightParentView.width;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - actions
+
+- (void)onSwitchToDoubleFlightButtonTap:(id)sender
+{
+	[UIView animateWithDuration:0.3f
+					 animations:^{
+						 self.doubleFlightParentView.left = 0;
+						 self.singleFlightParentView.left = -self.singleFlightParentView.width;
+					 }
+					 completion:^(BOOL finished) {
+					 }];
+}
+
+- (void)onSwitchToSingleFlightButtonTap:(id)sender
+{
+	[UIView animateWithDuration:0.3f
+					 animations:^{
+						 self.doubleFlightParentView.left = self.singleFlightParentView.width;
+						 self.singleFlightParentView.left = 0;
+					 }
+					 completion:^(BOOL finished) {
+					 }];
+}
+
+#pragma mark - tableview methods
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	UITableViewCell* result;
+	
+	if (tableView == self.singleFlightTableView) {
+		if (indexPath.row == 0) {
+			result = [[[NSBundle mainBundle] loadNibNamed:@"FlightSearchCells" owner:nil options:nil] objectAtIndex:0];
+		} else if (indexPath.row == 1) {
+			result = [[[NSBundle mainBundle] loadNibNamed:@"FlightSearchCells" owner:nil options:nil] objectAtIndex:1];
+		} else {
+			result = [[[NSBundle mainBundle] loadNibNamed:@"FlightSearchCells" owner:nil options:nil] objectAtIndex:2];
+		}
+	} else {
+		if (indexPath.row == 0) {
+			result = [[[NSBundle mainBundle] loadNibNamed:@"FlightSearchCells" owner:nil options:nil] objectAtIndex:0];
+		} else if (indexPath.row == 1) {
+			result = [[[NSBundle mainBundle] loadNibNamed:@"FlightSearchCells" owner:nil options:nil] objectAtIndex:1];
+		} else if (indexPath.row == 2) {
+			result = [[[NSBundle mainBundle] loadNibNamed:@"FlightSearchCells" owner:nil options:nil] objectAtIndex:2];
+		} else {
+			result = [[[NSBundle mainBundle] loadNibNamed:@"FlightSearchCells" owner:nil options:nil] objectAtIndex:3];
+		}
+	}
+	
+	return result;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	if (tableView == self.singleFlightTableView)
+		return 3;
+	else
+		return 4;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	
+	
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
