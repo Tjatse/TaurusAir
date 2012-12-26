d19788ea223a3f033a7526aa131c0286bcb77dbb
Simon <yqqjerk@yangmatoMacBook-Pro.local>
2012-12-26 11:33:38 +0800

    geocode helper

---
+++ Data/Utils/GeocodeHelper.h
@@ -0,0 +1,19 @@
+//
+//  GeocodeHelper.h
+//  TaurusClient
+//
+//  Created by Simon on 12-12-26.
+//  Copyright (c) 2012年 Taurus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^OnGeocodeResponse)(NSString* address);
+
+@interface GeocodeHelper : NSObject
+
++ (void)requestGeocodeWithLat:(float)lat
+					   andLon:(float)lon
+				  andComplete:(OnGeocodeResponse)complete;
+
+@end
