0a5f9107f4de6bcdf085ed018744b9ee0720440c
Simon <yqqjerk@bogon.(none)>
2013-01-02 07:45:23 +0800

    .

---
+++ TaurusClient/FlightNotification/FlightNotificationViewController.m
@@ -0,0 +1,38 @@
+//
+//  FlightNotificationViewController.m
+//  TaurusClient
+//
+//  Created by Simon on 13-1-2.
+//  Copyright (c) 2013年 Taurus. All rights reserved.
+//
+
+#import "FlightNotificationViewController.h"
+
+@interface FlightNotificationViewController ()
+
+@end
+
+@implementation FlightNotificationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
