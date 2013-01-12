238f6dfd970da8a2cc8ff5afdaabe726497ead54
Simon <yqqjerk@yangmatoMacBook-Pro.local>
2013-01-12 13:25:36 +0800

    airplane type model

---
+++ Data/Entity/AirplaneType.h
@@ -0,0 +1,13 @@
+//
+//  AirplaneType.h
+//  TaurusClient
+//
+//  Created by Simon on 13-1-12.
+//  Copyright (c) 2013年 Taurus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AirplaneType : NSObject
+
+@end
