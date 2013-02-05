c5fee23cb1224a5b7ad6c782a710e93d396e53b1
Simon <yqqjerk@yangmatoMacBook-Pro.local>
2013-02-05 20:56:39 +0800

    feedback

--- Data/AppDefines.h
+++ Data/AppDefines.h
@@ -58,6 +58,9 @@ extern NSString* const		kAlixPaySellerId;
 extern NSString* const		kAlixPayRSASafeCode;
 extern NSString* const		kAlixPayRSAPublicKey;
 
+// feedback
+extern NSString* const		kFeedbackURL;
+
 #define setRequestAuth(request) \
 		[request setPostValue:CLIENT_ID forKey:@"ClientId"]; \
 		[request setPostValue:SAFE_CODE forKey:@"safeCode"];
